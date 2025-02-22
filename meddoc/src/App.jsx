b33089d3b9fab0f42314710610c93674ed7ff89e
Chaerin Lee <chaerinlee1215@gmail.com>
2025-02-22 17:56:14 -0500

    login page changes frontend

--- meddoc/src/App.jsx
+++ meddoc/src/App.jsx
@@ -5,22 +5,24 @@ import Drex from "./assets/DRex.png";
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
-      {/* Left Section (Image) */}
-      <div className="flex-1 flex flex-col justify-center items-center bg-white p-8">
-        <h1 className="text-5xl font-bold text-green-600 mb-6">
+      {/* Left Section */}
+      <div className="flex-1 flex flex-col justify-between items-start bg-white p-8 relative">
+        <h1 className="text-4xl font-semibold text-[#879B87] mb-6">
           med<span className="text-black">Doc</span>
         </h1>
-        <div className="w-full max-w-md flex justify-center">
+
+        <div className="w-full max-w-2xl absolute bottom-0 left-12">
           <img
             src={Drex}
             alt="medDoc Mascot"
-            className="w-auto h-auto max-h-[400px] object-contain"
+            className="w-full h-auto object-contain"
           />
         </div>
       </div>
 
-      {/* Right Section (Login Form) */}
-      <div className="flex-1 flex justify-center items-center bg-green-100 p-8">
+
+      {/* Right Section */}
+      <div className="flex-1 flex justify-center items-center p-8" style={{ backgroundColor: '#CAE2CA' }}>
         <LoginForm />
       </div>
     </div>
